0c5fb54c99aa9c969ab049b038dceb7b0929c356
jjackson85 <jjackson8@gardner-webb.edu>
2021-06-28 19:25:31 +0000

    Created the index.html file

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+/*
+Jonathan Jackson
+script.js
+06/28/2021 */
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+}
